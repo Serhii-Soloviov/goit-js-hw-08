297bf37363a0587824e7194ba5ebb6b34cc33c3b
Serhii Soloviov <115696868+Serhii-Soloviov@users.noreply.github.com>
2023-01-22 02:44:59 +0200

    'update'

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+  )
+  .join('');
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+// console.log(markup);
+
+galleryRef.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(evt) {
+  evt.preventDefault();
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionDelay: `250`,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
 
+import throttle from 'lodash.throttle';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  labelEmail: document.querySelector('input'),
+  labelMessage: document.querySelector('textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.labelEmail.addEventListener('input', throttle(onFormAddEmail, 500));
+refs.labelMessage.addEventListener('input', throttle(onFormAddMessage, 500));
+
+populateFormData();
+
+function onFormSubmit(elem) {
+  elem.preventDefault();
+
+  if (refs.labelEmail.value === '' || refs.labelMessage.value === '') {
+    alert('Будь ласка, заповніть всі поля');
+  } else {
+    elem.target.reset();
+    localStorage.removeItem('email-value');
+    localStorage.removeItem('message-value');
+  }
+}
+
+function onFormAddEmail(elem) {
+  const emailData = elem.target.value;
+  localStorage.setItem('email-value', emailData);
+}
+
+function onFormAddMessage(elem) {
+  const messageData = elem.target.value;
+  localStorage.setItem('message-value', messageData);
+}
+
+function populateFormData() {
+  const savedEmail = localStorage.getItem('email-value');
+  const savedMessage = localStorage.getItem('message-value');
+
+  if (savedEmail || savedMessage) {
+    refs.labelEmail.value = savedEmail;
+    refs.labelMessage.value = savedMessage;
+  }
+}
